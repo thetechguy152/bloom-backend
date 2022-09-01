b755fa76535070237d0cfff24877b18c9e002b32
Ezinwoke Chukwukere Obidi <thetechguy152@gmail.com>
2022-09-01 04:47:26 +0100

    first6 commit

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -3,7 +3,6 @@ module.exports = (mongoose, mongoosePaginate) => {
     {
       name: {type: String, unique: true, required: true },
       email: { type: String, unique: true, required: true },
-      phone: {type: String, unique: true, required: true },
       gender: {type: String, unique: true, required: true },
       password: {type: String, required: true },
       roles: [
